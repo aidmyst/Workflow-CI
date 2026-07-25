ec74eaa48e756950a3d2b55fb691270cad397c36
Syahid-IT <dihays125@gmail.com>
2026-07-26 04:15:42 +0700

    Change dataset from Wine Quality to Diabetes

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,15 +1,17 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+# pyrefly: ignore [missing-import]
 import mlflow
+# pyrefly: ignore [missing-import]
 import mlflow.sklearn
 from sklearn.model_selection import train_test_split
 
 mlflow.autolog()
 
-df = pd.read_csv('namadataset_preprocessing/wine_quality_preprocessing.csv')
-X = df.drop('quality', axis=1)
-y = df['quality']
+df = pd.read_csv('namadataset_preprocessing/diabetes_preprocessing.csv')
+X = df.drop('target', axis=1)
+y = df['target']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 with mlflow.start_run():
